9e1d119ac1db9af222b71fa79797de6919852999
rustam <rustamaxm@gmail.com>
2025-08-20 16:11:32 +0300

    add demo params

--- automation_basics/test_demo/test_demo.py
+++ automation_basics/test_demo/test_demo.py
@@ -2,6 +2,8 @@
 Тут фаил с тестами.
 
 """
+import pytest
+
 
 def test_randon(get_testing_data):
     """
@@ -27,3 +29,16 @@ def test_randon_false(get_testing_data):
     val = get_testing_data
     print(f"{val=}")
     assert not val > 1.0
+
+def test_fixture_param(get_params):
+    """
+    параметризованный тест - фикстура с разными параметрами
+    """
+    assert get_params > 2
+
+@pytest.mark.parametrize('a,b', [(1, 1), (2, 4), (3, 3)])
+def test_params_in_test(a, b):
+    """
+    сам тест с разными параметрами
+    """
+    assert a == b
